6cc002564e7174b5829c79660ea9edde4a45ff77
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-05-26 02:35:08 +0800

    avoid warning on main
    and output the n:

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 extern crate fizzybuzz;
 
+#[cfg_attr(test, allow(dead_code))]
 fn main() {
     println!("Fizz Buzz!");
 
     for x in 1..25 {
-      println!("|{}|", fizzybuzz::word_for(x)); // x: i32
+      println!("{}: {}", x, fizzybuzz::word_for(x)); // x: i32
   }
 }
